3cabbc3616428a3ecd945e91c86899523af4020d
Nadav Tasher <tashernadav@gmail.com>
2024-10-12 22:45:58 +0300

    Added exports

--- src/rednest/__init__.py
+++ src/rednest/__init__.py
@@ -7,3 +7,6 @@ from rednest.dictionary import Dictionary
 
 # Import base objects for extendability
 from rednest.nested import Nested, NestedType, NESTED_TYPES
+
+# Set exported names
+__all__ = ["Encoder", "List", "Dictionary", "Nested", "NestedType", "NESTED_TYPES"]
